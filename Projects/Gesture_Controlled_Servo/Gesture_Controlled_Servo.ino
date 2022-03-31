22b32177ba42a76098c2032675bae188b4f6426d
sYarbar <68980165+sYarbar@users.noreply.github.com>
2022-03-30 19:42:59 -0500

    Update Gesture_Controlled_Servo.ino

--- Projects/Gesture_Controlled_Servo/Gesture_Controlled_Servo.ino
+++ Projects/Gesture_Controlled_Servo/Gesture_Controlled_Servo.ino
@@ -1,8 +1,20 @@
+/*
+-------------------------------------------------------------------------------
+Gesture_Controlled_Servo
+The hardware for this code is a photoresistor connected to a0 and 
+a servo attached to pin 9. 
+
+The light sensor data should change in response to someone's hand approaching
+the sensor. The motor will rotate between 0 and 180 degrees, based on how 
+close the hand is.
+-------------------------------------------------------------------------------
+*/
+
 #include <Servo.h>
-Servo myservo;//creates servo obj
-int pos = 0; // initiate servo position
+Servo myservo;
+int pos = 0; 
 const int analoglnPin = A0; //Make a name for A0
-int sensorValue = 0; //initiate sensor value 
+int sensorValue = 0; 
 int newPos = 0; //used for desired position using sensor data
 
 
@@ -15,7 +27,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
    sensorValue = analogRead(analoglnPin);
-   newPos = (sensorValue - 30)/110 * 180; //Max read is 140, lowest is 130. subtract 30 and take as fraction of 180 degrees
+   newPos = (sensorValue - 30)/110 * 180; //Max read is 140, lowest is 130. subtract 30 and take as fraction of 180 degrees.
+                                          //Converts sensor data to a 180 degree scale
    Serial.println(newPos);
 
   //moves servo towards desired position incrementally
